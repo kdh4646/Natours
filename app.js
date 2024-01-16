f151f3c85cacdd694530c5efbed22c0a1a588a36
Derek Kweon <kdh4646@hanmail.net>
2024-01-15 18:05:00 -0800

    2024-01-15 learning GET, POST

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const express = require('express');
 
 const app = express();
 
+//middleware
+app.use(express.json());
+
 // //HTTP GET
 // app.get('/', (req, res) => {
 //   res
@@ -32,6 +35,51 @@ app.get('/api/v1/tours', (req, res) => {
 
     data: { tours },
   });
+});
+
+//Get specific tour information
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  //convert to number
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  //validation
+  //if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
