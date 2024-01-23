a3fb43356eac520e5fcbc609bb8b987a710b8348
Derek Kweon <kdh4646@hanmail.net>
2024-01-22 17:52:11 -0800

    2024-01-22 learning creating schema and model

--- server.js
+++ server.js
@@ -10,11 +10,36 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+//DB connection - returns promise
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Connection Successful!');
+  });
+
+//creating schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
 });
+
+//creating model
+const Tour = mongoose.model('Tour', tourSchema);
 
 /* START SERVER */
 const port = process.env.PORT || 3000;
