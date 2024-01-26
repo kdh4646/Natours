20412f8127165c0e3eea093d167cf273eb26c7f3
Derek Kweon <kdh4646@hanmail.net>
2024-01-25 21:05:48 -0800

    2024-01-25 Learning Aggregation Pipeline             (Unwinding, Projecting, ...etc)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -122,3 +122,85 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+//Tour Stats
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' }, //group by
+          numTours: { $sum: 1 }, //count + 1
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1, //use names based on results from $group, 1 is for asc order
+        },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } }, //ne is not equal to
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+//Monthly Plan
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates', //consist separate elements from grouped data
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }, //return results in array that corresponds to given condition
+        },
+      },
+      { $addFields: { month: '$_id' } }, //add field based on given data
+      { $project: { _id: 0 } }, //0 for invisible, 1 for visible
+      { $sort: { numTourStarts: -1 } }, //-1 for dsc order
+      { $limit: 12 },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { plan },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
