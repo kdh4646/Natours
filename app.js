98c0dc8fa5e958514616a970b7c3a987086b3cbf
Derek Kweon <kdh4646@hanmail.net>
2024-02-04 18:04:14 -0800

    2024-02-04 against query injection and xss

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express'); //express
 const morgan = require('morgan'); //3rd part middleware - logging
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,6 +32,12 @@ app.use('/api', limiter);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); //limit the data
+
+//Data sanitization agains NoSQL query injection
+app.use(mongoSanitize()); //filter out all '$' and '.' signs
+
+//Data sanitization against XSS
+app.use(xss());
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
