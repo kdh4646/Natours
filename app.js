49388124a4597a587620dd5380bcbb1692a70f6f
Derek Kweon <kdh4646@hanmail.net>
2024-02-04 17:34:28 -0800

    2024-02-04 install express-rate-limit for security

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); //express
 const morgan = require('morgan'); //3rd part middleware - logging
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,12 +9,20 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-/* 1) MIDDLEWARES */
+/* 1) GLOBAL MIDDLEWARES */
 
 //works only when envrionment is development ex) to skip log-in
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //logging
 }
+
+//limit requests from one IP for security
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  mesage: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); //for static files
