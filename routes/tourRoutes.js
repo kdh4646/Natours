7ba5f99bf3d1f35d90972adc21c49a14a820ed07
Derek Kweon <kdh4646@hanmail.net>
2024-02-08 19:15:53 -0800

    2024-02-08 adding missing authentication

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -16,28 +16,35 @@ router
   .get(tourController.aliasTopTour, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('lead-guide', 'guide', 'admin'),
+    tourController.getMonthlyPlan,
+  );
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('lead-guide', 'admin'),
+    tourController.createTour,
+  );
+
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('lead-guide', 'admin'),
+    tourController.updateTour,
+  )
   .delete(
     authController.protect,
     authController.restrictTo('lead-guide', 'admin'),
     tourController.deleteTour,
   );
-
-// router
-//   .route('/:tourId/reviews')
-//   .post(
-//     authController.protect,
-//     authController.restrictTo('user'),
-//     reviewController.createReview,
-//   );
 
 module.exports = router;
