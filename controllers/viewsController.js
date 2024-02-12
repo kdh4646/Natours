03f18f6a308efd1584f9dd87d5f64a5130e3207b
Derek Kweon <kdh4646@hanmail.net>
2024-02-12 14:03:55 -0800

    2024-02-12 Building the tour overview w/ pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,18 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
 //Get Overview
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+
+  //2) Build template
+  //3) Render that template using tour data from 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 //Get Tour
 exports.getTour = (req, res) => {
